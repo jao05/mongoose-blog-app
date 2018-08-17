05b1e852cce56b57b2b2c240783dfb04f90ea623
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-08-17 17:03:42 -0400

    added PUT endpoint

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.get( '/posts', function( req, res ) {
 
 		Blog.find()    
-		    // success callback: for each restaurant we got back, we'll
+		    // success callback: for each blog we got back, we'll
 		    // call the `.serialize` instance method we've created in
 		    // models.js in order to only expose the data we want the API return.    
 		    .then(function(blogs) {
@@ -80,12 +80,39 @@ app.post("/posts", (req, res) => {
 });
 
 // PUT (UPDATE)
-app.put( '/posts/:id', function( req, res )
-				   {
-				   	
-				   } 
+app.put("/posts/:id", (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  // ***********lets talk about this******************
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message =
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
 
-);
+  // *************my blog app will allow updating of any field, but I want to discuss this...*********
+  
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ["title", "content", "author"];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      // ********Specifically, explain this syntax; seems a little diff than normal...class, array, what???**********
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    // ************Followed example, but why is parameter needed here????***********
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
+});
 
 // DELETE
 app.delete( '/posts/:id', function( req, res )
