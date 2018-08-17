bd970c9e83e017ca9507422644a72cc2902bcca3
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-08-17 16:35:05 -0400

    completed GET endpoints

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ mongoose.Promise = global.Promise;
 
 // config.js is where we control constants for entire
 // app like PORT and DATABASE_URL
-// ************FINISH ONCE THE MODEL HAS BEEN CREATED*********
+const { PORT, DATABASE_URL } = require("./config");
+const { Blog } = require("./models");
 
 // Instantiate an Express app
 const app = express();
@@ -21,20 +22,36 @@ app.use(express.json());
 // GET
 // Create a 'virtual' for the author's name string & 
 // serialize objects that are to be returned to API user
-app.get( '/posts', function( req, res )
-				   {
+app.get( '/posts', function( req, res ) {
 
-				   } 
-
+		Blog.find()    
+		    // success callback: for each restaurant we got back, we'll
+		    // call the `.serialize` instance method we've created in
+		    // models.js in order to only expose the data we want the API return.    
+		    .then(function(blogs) {
+		      res.json({
+		        blogs: blogs.map(blog => blog.serialize())
+		      });
+		    })
+		    .catch(err => {
+		      console.error(err);
+		      res.status(500).json({ message: "Internal server error" });
+		    });
+	} 
 );
 
 // GET by ID
-app.get( '/posts/:id', function( req, res )
-				   {
-
-				   } 
-
-);
+app.get("/posts/:id", (req, res) => {
+  Blog
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(blog => res.json(blog.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
 
 // POST (CREATE)
 app.post( '/posts', function( req, res )
