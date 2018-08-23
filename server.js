befadfdc09ccd7ffee2306f3e72179ac938b8846
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-08-23 19:04:24 -0400

    added endpoints for Author requests

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Import Express
 const express = require("express");
 
+// Import Morgan
+const morgan = require("morgan");
+
 // Import Mongoose
 const mongoose = require("mongoose");
 
@@ -11,7 +14,7 @@ mongoose.Promise = global.Promise;
 // config.js is where we control constants for entire
 // app like PORT and DATABASE_URL
 const { PORT, DATABASE_URL } = require("./config");
-const { Blog } = require("./models");
+const { Author, Blog } = require("./models");
 
 // Instantiate an Express app
 const app = express();
@@ -19,7 +22,133 @@ const app = express();
 // Use JSON is this app
 app.use(express.json());
 
-// GET
+// Use Morgan is this app
+app.use(morgan('common'));
+
+// GET for authors
+app.get('/authors', (req, res) => {
+  Author
+    .find()
+    .then(authors => {
+      res.json(authors.map(author => {
+        return {
+          id: author._id,
+          name: `${author.firstName} ${author.lastName}`,
+          userName: author.userName
+        };
+      }));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
+});
+
+// POST for authors
+app.post('/authors', (req, res) => {
+  const requiredFields = ['firstName', 'lastName', 'userName'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+
+  Author
+    .findOne({ userName: req.body.userName })
+    .then(author => {
+      if (author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName
+          })
+          .then(author => res.status(201).json({
+              _id: author.id,
+              name: `${author.firstName} ${author.lastName}`,
+              userName: author.userName
+            }))
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went wrong' });
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went horribly awry' });
+    });
+});
+
+// PUT for authors
+app.put('/authors/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['firstName', 'lastName', 'userName'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  Author
+    //*******explain syntax here**********
+    .findOne({ userName: updated.userName || '', _id: { $ne: req.params.id } })
+    .then(author => {
+      if(author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+          .then(updatedAuthor => {
+            res.status(200).json({
+              id: updatedAuthor.id,
+              name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+              userName: updatedAuthor.userName
+            });
+          })
+          .catch(err => res.status(500).json({ message: err }));
+      }
+    });
+});
+
+
+// DELETE for authors
+app.delete('/authors/:id', (req, res) => {
+  // First, remove all blogposts by this author from the Blog collection
+  BlogPost
+    .remove({ author: req.params.id })
+    .then(() => {
+      // Then, remove the author from the "Author" collection
+      Author
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+          console.log(`Deleted blog posts owned by and author with id \`${req.params.id}\``);
+          res.status(204).json({ message: 'success' });
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
+});
+
+// GET for blogposts
 // Create a 'virtual' for the author's name string & 
 // serialize objects that are to be returned to API user
 app.get( '/posts', function( req, res ) {
@@ -40,7 +169,7 @@ app.get( '/posts', function( req, res ) {
 	} 
 );
 
-// GET by ID
+// GET by ID for blogposts
 app.get("/posts/:id", (req, res) => {
   Blog
     // this is a convenience method Mongoose provides for searching
@@ -53,7 +182,7 @@ app.get("/posts/:id", (req, res) => {
     });
 });
 
-// POST (CREATE)
+// POST (CREATE) for blogposts
 app.post("/posts", (req, res) => {
   const requiredFields = ["title", "content", "author"];
   for (let i = 0; i < requiredFields.length; i++) {
@@ -67,9 +196,8 @@ app.post("/posts", (req, res) => {
 
   Blog.create({
     title: req.body.title,
-    content: req.body.content,
+    content: req.body.content,    
     
-    // **********How do we parse both the author's firstName & lastName???*********
     author: req.body.author    
   })
     .then(blog => res.status(201).json(blog.serialize()))
@@ -79,10 +207,9 @@ app.post("/posts", (req, res) => {
     });
 });
 
-// PUT (UPDATE)
+// PUT (UPDATE) for blogposts
 app.put("/posts/:id", (req, res) => {
-  // ensure that the id in the request path and the one in request body match
-  // ***********lets talk about this******************
+  // ensure that the id in the request path and the one in request body match  
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message =
       `Request path id (${req.params.id}) and request body id ` +
@@ -91,8 +218,6 @@ app.put("/posts/:id", (req, res) => {
     return res.status(400).json({ message: message });
   }
 
-  // *************my blog app will allow updating of any field, but I want to discuss this...*********
-  
   // we only support a subset of fields being updateable.
   // if the user sent over any of the updatableFields, we udpate those values
   // in document
@@ -100,24 +225,21 @@ app.put("/posts/:id", (req, res) => {
   const updateableFields = ["title", "content", "author"];
 
   updateableFields.forEach(field => {
-    if (field in req.body) {
-      // ********Specifically, explain this syntax; seems a little diff than normal...class, array, what???**********
+    if (field in req.body) {      
       toUpdate[field] = req.body[field];
     }
   });
 
   Blog
     // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    // ************Followed example, but why is parameter needed here????***********
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })    
     .then(blog => res.status(204).end())
     .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-// DELETE
+// DELETE for blogposts
 app.delete("/posts/:id", (req, res) => {
-  Blog.findByIdAndRemove(req.params.id)
-    // ************Followed example, but why is parameter needed here???? Same as above in PUT...***********
+  Blog.findByIdAndRemove(req.params.id)    
     .then(blog => res.status(204).end())
     .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
